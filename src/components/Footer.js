c87d306941fd740ccfe488e639114db9efd9045d
martinLeht <martin.lehtomaa@gmail.com>
2021-06-10 11:57:54 +0300

    Added deployment to netlify

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,11 +41,6 @@ class Footer extends Component {
                     </MDBCol>
                     </MDBRow>
                 </MDBContainer>
-                <div className="footer-copyright text-center py-3">
-                    <MDBContainer fluid>
-                    &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
-                    </MDBContainer>
-                </div>
             </MDBFooter>
         )
     }
